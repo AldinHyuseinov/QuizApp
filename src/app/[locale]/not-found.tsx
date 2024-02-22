351c73eaa7a4331552250347f61e66d00265a1f8
AldinHyuseinov <Aldin6687@gmail.com>
2024-02-22 15:41:26 +0200

    translations for not found page

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,10 +1,14 @@
+import { useTranslations } from "next-intl";
+
 // `app/[locale]/[...rest]/page.tsx`
 // is necessary for this page to render.
 export default function NotFoundPage() {
+  const t = useTranslations("NotFoundPage");
+
   return (
     <main className="flex flex-col items-center">
-      <h1 className="max-w-[460px]">Not Found</h1>
-      <p className="text-2xl">Sorry, the page you&apos;re looking for can&apos;t be found.</p>
+      <h1 className="max-w-[460px]">{t("title")}</h1>
+      <p className="text-2xl">{t("paragraph")}</p>
     </main>
   );
 }
